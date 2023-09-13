486440645855771027d3b33b1f699c790e917609
alsensto <jmg1301@gmail.com>
2023-09-13 15:03:43 +0900

    1

--- src/main/java/com/example/user/UserApplication.java
+++ src/main/java/com/example/user/UserApplication.java
@@ -1,11 +1,17 @@
 package com.example.user;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserApplication {
-
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager){
+        return new JPAQueryFactory(entityManager);
+    }
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
